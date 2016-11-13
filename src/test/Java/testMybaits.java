9f2d5c9452e846f64b03d95ca261751cf7126856
cj <l>
2016-11-13 22:05:02 +0800

    as

--- src/test/Java/testMybaits.java
+++ src/test/Java/testMybaits.java
@@ -15,7 +15,7 @@ public class testMybaits {
     public void test1(){};
 
     public static void main(String[] args) {
-        System.out.println(231cv);
+        System.out.println(23134);
         ApplicationContext ctx=null;
        ctx=new ClassPathXmlApplicationContext("classpath:/spring-mybatis.xml");
         IUserDao userDao = ctx.getBean(IUserDao.class);
