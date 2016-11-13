bffe6af28eda51e727dd626461dfb6216a77fdc3
asas511 <1003413598@qq.com>
2016-11-13 22:01:45 +0800

    Update testMybaits.java

--- src/test/Java/testMybaits.java
+++ src/test/Java/testMybaits.java
@@ -15,7 +15,7 @@ public class testMybaits {
     public void test1(){};
 
     public static void main(String[] args) {
-        System.out.println(231);
+        System.out.println(2315);
         ApplicationContext ctx=null;
        ctx=new ClassPathXmlApplicationContext("classpath:/spring-mybatis.xml");
         IUserDao userDao = ctx.getBean(IUserDao.class);
